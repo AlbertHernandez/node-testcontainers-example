f0aa7092abebf11b675bdd0c88c4637d3be6f516
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2023-11-04 17:50:09 +0100

    feat: add beforeall timeout

--- tests/cache-client/redis-cache-client.test.ts
+++ tests/cache-client/redis-cache-client.test.ts
@@ -19,7 +19,7 @@ describe("RedisCacheClient", () => {
       url: redisUrl,
     });
     await redisCacheClient.connect();
-  });
+  }, 10000);
 
   afterAll(async () => {
     await redisCacheClient.disconnect();
